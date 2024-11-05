2661cbb52dc683718a6cc3119d1e5f1efd9dc6eb
Rikki <i@rikki.moe>
2024-11-05 16:54:29 +0800

    fix missed col

--- d1_init.sql
+++ d1_init.sql
@@ -1,5 +1,5 @@
-CREATE TABLE [files] ("key" text PRIMARY KEY, "mime" text, "filename" text, "filesize" integer, "created_at" integer, "expire_at" integer, "access_count" integer DEFAULT 0);
-CREATE TABLE [file_access] ("key" text, "ip" text, "country" text, "city" text, "ua" text, "referer" text, "access_at" integer, FOREIGN KEY(key) REFERENCES files(key) ON DELETE CASCADE);
+CREATE TABLE IF NOT EXISTS [files] ("key" text PRIMARY KEY, "mime" text, "filename" text, "filesize" integer, "created_at" integer, "expire_at" integer, "access_count" integer DEFAULT 0);
+CREATE TABLE IF NOT EXISTS [file_access] ("key" text, "ip" text, "country" text, "city" text, "ua" text, "referer" text, "access_at" integer, FOREIGN KEY(key) REFERENCES files(key) ON DELETE CASCADE);
 
-CREATE TABLE [links] ("key" text PRIMARY KEY, "destination" text, "created_at" integer, "expire_at" integer, "access_count" integer DEFAULT 0);
-CREATE TABLE [link_access] ("key" text, "ip" text, "country" text, "city" text, "ua" text, "access_at" integer, FOREIGN KEY(key) REFERENCES links(key) ON DELETE CASCADE);
+CREATE TABLE IF NOT EXISTS [links] ("key" text PRIMARY KEY, "destination" text, "created_at" integer, "expire_at" integer, "access_count" integer DEFAULT 0);
+CREATE TABLE IF NOT EXISTS [link_access] ("key" text, "ip" text, "country" text, "city" text, "ua" text, "referer" text, "access_at" integer, FOREIGN KEY(key) REFERENCES links(key) ON DELETE CASCADE);
